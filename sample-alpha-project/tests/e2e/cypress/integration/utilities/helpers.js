c6ed7fec7d5627403dfa8ac4ea0ff758f890677d
Jeff <jeffgersfx@gmail.com>
2019-11-03 23:07:47 -0700

    Added a condition to setup() that pauses tests with an error message if rate limited
    There's no way to run the suite if rate limited, wait for it to reset and try again.

--- sample-alpha-project/tests/e2e/cypress/integration/utilities/helpers.js
+++ sample-alpha-project/tests/e2e/cypress/integration/utilities/helpers.js
@@ -7,6 +7,11 @@ export const help = {
         cy.visit('/' + '/commits', {timeout: 2000})
         cy.clearCookies()
         cy.clearLocalStorage()
+
+        if (cy.get('.text-xs-center').contains('No data available')) {
+            cy.log('YOU ARE RATE LIMITED - NO TESTS WILL RUN - CYPRESS WILL BE PAUSED - CHECK BACK FOR RATE LIMIT RESET')
+            cy.pause()
+        }
     },
 
     //Taps a button based on the string passed through to the parameter
